cc58f57c6135bd8897c2806c71591e767a1a307c
Manuel Huerta <manuel.gat1@gmail.com>
2020-12-09 09:10:18 -0600

    Agregado navbar a perfil

--- Torneos/app-torneos/src/main/java/mx/uv/App.java
+++ Torneos/app-torneos/src/main/java/mx/uv/App.java
@@ -4,10 +4,25 @@ package mx.uv;
  * Hello world!
  *
  */
+
+import static spark.Spark.*;
+
+import spark.Spark.*;
+import spark.Request;
+import spark.Response;
+import spark.ModelAndView;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
+        HashMap<String, String> model = new HashMap<>();
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Aloha");
     }
 }

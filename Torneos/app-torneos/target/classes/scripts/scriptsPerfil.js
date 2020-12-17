d9b86f7f76711840701d0c129e3b5f5e7e61098f
Manuel Huerta <manuel.gat1@gmail.com>
2020-12-17 09:34:58 -0600

    Modificar foto de perfil y cambios esteticos

---
+++ Torneos/app-torneos/target/classes/scripts/scriptsPerfil.js
@@ -0,0 +1,9 @@
+var bNombreUsuario = document.getElementById("nombreP").innerHTML = "EteSech";
+
+var bEmail = document.getElementById("emailP").innerHTML = "correo@yes.no";
+
+
+
+$('#editarPerfil').click(function() {
+    window.location='editarPerfil.html';
+  });
